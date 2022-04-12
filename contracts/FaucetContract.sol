2e9027bdc83d95489e4ead6ffd9679d105aa3972
LAPTOP-D7FTRIR6\andre <andrew.persaud.dev@gmail.com>
2022-04-11 21:33:55 -0400

    Added withdraw function to faucet

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -1,10 +1,72 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
-  //storage variables
-  uint public funds = 1000;
-  int public counter = -10;
-  uint32 test = 4294967295;
+
+  address public owner;
+  uint public numOfFunders;
+  mapping(address => bool) private funders; 
+  mapping(uint => address) private lutFunders;
+
+  constructor() {
+    owner = msg.sender;
+  }
+
+  modifier limitWithdraw(uint withdrawAmount) {
+    require(
+      withdrawAmount <= 2000000000000000000, 
+      "Can only withdraw 2 eth or less"
+    );
+
+    _;  //run function body if condition met
+  }
+
+  modifier onlyOwner {
+    require (
+      msg.sender == owner,
+      "Only owner is authorized to call this function."
+    );
+
+    _;
+  }
 
   
+  receive() external payable {}
+
+  function addFunds() external payable {
+    address funder = msg.sender;
+    if (!funders[funder]) {
+      funders[funder] = true;
+      lutFunders[numOfFunders++] = funder;
+    }
+
+  }
+
+  function withdraw(uint withdrawAmount) external limitWithdraw(withdrawAmount) {
+    payable(msg.sender).transfer(withdrawAmount);
+    
+  }
+
+  function getAllFunders() external view returns (address[] memory) {
+    address[] memory _funders = new address[](numOfFunders);
+    for (uint i = 0; i < numOfFunders; i++) {
+      _funders[i] = lutFunders[i];
+    }
+    return _funders;
+  }
+
+
+
+  function getFunderAtIndex(uint8 index) external view returns(address) {
+    return lutFunders[index];
+  }
 }
+
+
+
+
+// const instance = await Faucet.deployed()
+
+// instance.addFunds({ from: accounts[0], value: "2000000000000000000" })  
+// instance.withdraw("2000000000000000000", { from: accounts[1] })
+//instance.getFunderAtIndex()
+//instance.getAllFunders()
